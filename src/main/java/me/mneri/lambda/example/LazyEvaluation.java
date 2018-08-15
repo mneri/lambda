f96db616afb886aca008df7cf506597f6861be12
Massimo Neri <hello@mneri.me>
2018-08-15 14:26:58 +0200

    LazyEvaluation demo

--- src/main/java/me/mneri/lambda/example/LazyEvaluation.java
+++ src/main/java/me/mneri/lambda/example/LazyEvaluation.java
@@ -1,4 +1,15 @@
 package me.mneri.lambda.example;
 
+import me.mneri.lambda.λ;
+
+import static me.mneri.lambda.Lambdas.*;
+import static me.mneri.lambda.Redex.β;
+
 public class LazyEvaluation {
+    public static void main(String... args) {
+        λ DIE = β(IF, TRUE, I, β(Ω, Ω));
+        λ r   = DIE.compute();
+
+        System.out.println("Not dead");
+    }
 }
