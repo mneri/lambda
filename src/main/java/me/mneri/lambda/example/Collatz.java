046813bdda747554fa23aa166c3a8a6c971d199f
Massimo Neri <hello@mneri.me>
2018-08-17 02:50:17 +0200

    Collatz

--- src/main/java/me/mneri/lambda/example/Collatz.java
+++ src/main/java/me/mneri/lambda/example/Collatz.java
@@ -1,4 +1,20 @@
 package me.mneri.lambda.example;
 
+import me.mneri.lambda.λ;
+
+import static me.mneri.lambda.Lambdas.*;
+import static me.mneri.lambda.Redex.β;
+
 public class Collatz {
+    public static void main(String... args) {
+        //@formatter:off
+        λ COLLATZ = β(Y, (λ f) -> (λ n) -> β(IF, β(EQ, n, ONE),
+                                                 I,
+                                                 β(f, β(IF, β(ISEVEN, n),
+                                                            β(DIV, n, TWO),
+                                                            β(SUCC, β(MUL, n, THREE))))));
+        //@formatter:on
+
+        β(COLLATZ, THIRTEEN).compute();
+    }
 }
